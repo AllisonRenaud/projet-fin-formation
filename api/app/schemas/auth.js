433a7cdf13bf22d29741a86ee6f30c2cf997af9c
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-09-24 10:38:00 +0200

    auth data schema refacto

--- api/app/schemas/auth.js
+++ api/app/schemas/auth.js
@@ -14,13 +14,6 @@ module.exports = {
         firstname: Joi.string().max(40).trim().required(),
         lastname: Joi.string().max(40).trim().required(),
         email: Joi.string().email().max(50).trim().lowercase().required(),
-        phone: Joi.string().max(10).pattern(/(^0)([0-9])+$/),
-        birth_date: Joi.date().max(cutoffDate),
-        zip_code: joiPostalCode.string().postalCode("FR"),
-        city_name: Joi.string().max(50).trim(),
-        country: Joi.string().max(40).trim(),
-        street_name: Joi.string().trim(),
-        street_number: Joi.string().alphanum().max(10).trim(),
         password: Joi.string().trim().required().regex(new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]){8,}$')),
         passwordConfirm: Joi.string().trim().required().valid(Joi.ref('password'))
 
