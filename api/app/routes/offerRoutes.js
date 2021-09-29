1d4b76a56acf45f56aa7ea42c786df5fc047b714
BenjaminChoron <benjamin.choron@outlook.com>
2021-09-29 12:24:08 +0200

    change method on offers route

--- api/app/routes/offerRoutes.js
+++ api/app/routes/offerRoutes.js
@@ -3,7 +3,7 @@ const offerController = require("../controllers/offerController")
 
 
 offerRouter.route("/offers")
-.get(offerController.findByLocation)
+.get(offerController.findAllOrFilter)
 
 offerRouter.route("/offers/:id")
 .get(offerController.findById)
