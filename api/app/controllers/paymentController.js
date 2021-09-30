13434d417e4c7fedb94c661b62c20b5ee48b7cc7
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-09-30 18:21:10 +0200

    stripe init not done

--- api/app/controllers/paymentController.js
+++ api/app/controllers/paymentController.js
@@ -23,36 +23,21 @@ const paymentController = {
 
           // })
 
-          // response.json(session)
-
+          
 
 
           // const paymentIntent = await stripe.paymentIntents.create({
-          //   amount: session,
-          //   currency: 'eur',
-          //   payment_method_types: ['card'],
-          //   receipt_email: 'jenny.rosen@example.com',
-          // });
-       
-          
-          // console.log(intent , intent.charges.data )
-          // const {url, id} = session
-          
-       
-          // const paymentIntent = await stripe.paymentIntents.create({
-          //   amount: 1000,
+          //   amount: session.amount_total,
           //   currency: 'eur',
           //   description: 'Ochalet - location de chalet premium',
-          //   payment_method: 'cus_KJz9njOYn2sfcT',
           //   payment_method_types: ['card'],
-          //   confirm: true,
           //   receipt_email: 'ochaleto@gmail.com'
           // });
 
+          // response.json([session, paymentIntent])
 
-          // response.json(paymentIntent)
-
-        
+          const intent = await stripe.paymentIntents.retrieve("pi_3JfRtgKVmN7kqniy1IpOMll2");
+          response.json(intent)
          
         } catch(error) {
             response.status(500).send(error.message);
