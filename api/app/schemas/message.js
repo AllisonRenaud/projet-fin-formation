c1d0300d22099f829b0375532e84d4b328b6e697
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-13 00:17:47 +0200

    message controller clean

--- api/app/schemas/message.js
+++ api/app/schemas/message.js
@@ -5,24 +5,13 @@ module.exports = {
     
     
     createMessage: Joi.object({
-        reservation_start: Joi.date(),
-        reservation_end: Joi.date(),
-        nb_persons: Joi.number().positive(),
         body: Joi.string().trim().required(),
         offer_id: Joi.number().positive().required(),
-        message_status: Joi.boolean(),
-
     }),
 
     updateMessage: Joi.object({
       id: Joi.number().required(),
-      reservation_start: Joi.date(),
-      reservation_end: Joi.date(),
-      nb_persons: Joi.number().positive(),
-      body: Joi.string().trim(),
-      offer_id: Joi.number().positive().required(),
-      message_status: Joi.boolean(),
-      user_id: Joi.number().positive().required()
+      message_status: Joi.boolean().required(),
     }),
 
     deleteMessage: Joi.object({
