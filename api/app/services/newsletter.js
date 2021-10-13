9eb17c0ee83178f151354b52ebd4a4d53d824ea1
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-13 02:59:37 +0200

    newsletter clean

--- api/app/services/newsletter.js
+++ api/app/services/newsletter.js
@@ -1,28 +1,26 @@
-const db = require('../databases/postgres')
-const sendEmail = require('./nodemailer')
-const newsLetterTemplate = require('../utils/email-templates/newsletterTemplate')
+const db = require('../databases/postgres');
+const sendEmail = require('./nodemailer');
+const newsLetterTemplate = require('../utils/email-templates/newsletterTemplate');
 
 
 module.exports = async (request, response) => {
   try {
 
-    const {rows} = await db.query('SELECT "email", "lastname", "firstname" FROM "user"')
-    if(!rows.length) return response.status(404).send({error: "no user found in database"})
+    const {rows} = await db.query('SELECT "email", "lastname", "firstname" FROM "user"');
+    if(!rows.length) return response.status(404).send({error: "no user found in database"});
     const users = rows.map(user => {
-      return {name: `${user.firstname} ${user.lastname}`, email: user.email}
-    })
-    for (user of users) {
-      const emailBody = newsLetterTemplate(user)
-      await sendEmail(user.email, "Super promo O'chalet", emailBody)
-    }
-    response.json({message: "newsletter sent"})
-    
+      return {name: `${user.firstname} ${user.lastname}`, email: user.email};
+    });
+    for (const user of users) {
+      const emailBody = newsLetterTemplate(user);
+      await sendEmail(user.email, "Super promo O'chalet", emailBody);
+    };
+    response.json({message: "newsletter sent"});
 
   } catch (error) {
     response.status(500).send({ error: error.message })
-    console.log(error.message)
   }
-}
+};
 
 
 
